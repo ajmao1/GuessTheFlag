f39d170bb75423ee5dc58e3aa1a381cd6de7201e
ajmao1 <maoaj22@gmail.com>
2025-11-10 18:55:32 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by AJ Mao on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
